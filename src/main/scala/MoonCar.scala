1d1cdc245ab93f167307ec90104e7056f7e5a68b
szywind@163.com <8891@Szy>
2017-02-15 19:31:27 -0600

    clean codes

--- src/main/scala/MoonCar.scala
+++ src/main/scala/MoonCar.scala
@@ -4,27 +4,37 @@ import akka.actor.ActorRef
   * Created by 1 on 2/13/17.
   */
 class MoonCar(_id: Int, _cur: Point, _end: Point, _v: Double, _angle: Double, _callback: ActorRef){
+    // 车辆编号
     def id = _id
+    // 当前位置
     var cur = _cur
+    // 终点位置
     def end = _end
+    // 速度
     var v = _v
+    // 方向角（相对于+x轴，逆时针为正值）
     var angle = _angle
+    // event-driven handler
     def callback = _callback
+
     sendOnCreate()
 
     def done() = {
         cur == end
     }
+
+    // 平移
     def move() = {
         cur = cur.move(v, angle)
     }
 
+    // 旋转
     def rotate(r_angle: Double) = {
         angle += r_angle
         sendOnRotation(r_angle)
-
     }
 
+    // 变速
     def accelerate(d_v: Double) = {
         v += d_v
     }
